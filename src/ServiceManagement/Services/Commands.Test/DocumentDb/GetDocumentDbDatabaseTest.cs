ab2380c5b4e244a59c02346de96b6921288c8efe
Nick Heppleston <nick@modhul.com>
2015-07-23 22:30:04 +0100

    Added unit tests for the GetDocumentDbDatabase Command.

--- src/ServiceManagement/Services/Commands.Test/DocumentDb/GetDocumentDbDatabaseTest.cs
+++ src/ServiceManagement/Services/Commands.Test/DocumentDb/GetDocumentDbDatabaseTest.cs
@@ -17,16 +17,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Management.Automation;
 using Microsoft.WindowsAzure.Commands.ScenarioTest;
-using Microsoft.WindowsAzure.Commands.Common;
 using Microsoft.WindowsAzure.Commands.Common.Test.Mocks;
 using Microsoft.WindowsAzure.Commands.DocumentDb;
 using Microsoft.WindowsAzure.Commands.Test.Utilities.Common;
-using Microsoft.WindowsAzure.Commands.Utilities.Properties;
 using Microsoft.WindowsAzure.Commands.Utilities.DocumentDb;
-using Xunit;
-using Moq;
 using Microsoft.Azure.Common.Authentication;
 using Microsoft.Azure.Documents;
+using Xunit;
+using Moq;
 
 namespace Microsoft.WindowsAzure.Commands.Test.DocumentDb
 {
@@ -53,7 +51,7 @@ namespace Microsoft.WindowsAzure.Commands.Test.DocumentDb
 
         [Fact]
         [Trait(Category.AcceptanceType, Category.CheckIn)]
-        public void GetDocumentDbDatabaseByIdSuccessful()
+        public void GetDocumentDbDatabaseById_ReturnsDatabase()
         {
             // Setup
             cmdlet.Id = "test-db";
@@ -64,13 +62,30 @@ namespace Microsoft.WindowsAzure.Commands.Test.DocumentDb
             cmdlet.ExecuteCmdlet();
 
             // Assert
-            var actual = mockCommandRuntime.OutputPipeline[0] as Database;
+            var actual = mockCommandRuntime.OutputPipeline[0] as ExtendedDocumentDbDatabase;
             Assert.Equal<string>(expected.Id, actual.Id);
         }
 
         [Fact]
         [Trait(Category.AcceptanceType, Category.CheckIn)]
-        public void GetDocumentDbDatabasesSuccessful()
+        public void GetDocumentDbDatabaseById_UnknownDatabaase_ReturnsWarning()
+        {
+            // Setup
+            cmdlet.Id = "test-db";
+            documentDbClientExtensionsMock.Setup(m => m.GetDatabase(It.IsAny<string>())).Returns<ExtendedDocumentDbDatabase>(null); // The specified DocumentDb Database cannot be found
+
+            // Test
+            cmdlet.ExecuteCmdlet();
+
+            // Assert
+            var actual = mockCommandRuntime.WarningStream[0];
+            Assert.False(String.IsNullOrEmpty(actual));
+            Assert.Equal<String>(String.Format("Could not locate DocumentDb Database with the Id '{0}'", cmdlet.Id), actual);
+        }
+
+        [Fact]
+        [Trait(Category.AcceptanceType, Category.CheckIn)]
+        public void GetDocumentDbDatabases_ReturnsListOfDatabases()
         {
             // Setup
             var expected = new List<ExtendedDocumentDbDatabase>() { new ExtendedDocumentDbDatabase { Id = "test-db-1" }, new ExtendedDocumentDbDatabase { Id = "test-db-2" } };
@@ -81,7 +96,7 @@ namespace Microsoft.WindowsAzure.Commands.Test.DocumentDb
             cmdlet.ExecuteCmdlet();
 
             // Assert
-            IEnumerable<Database> actual = LanguagePrimitives.GetEnumerable(mockCommandRuntime.OutputPipeline).Cast<Database>();
+            var actual = LanguagePrimitives.GetEnumerable(mockCommandRuntime.OutputPipeline).Cast<ExtendedDocumentDbDatabase>();
 
             Assert.NotNull(actual);
             Assert.Equal<int>(expected.Count, actual.Count());
@@ -93,14 +108,19 @@ namespace Microsoft.WindowsAzure.Commands.Test.DocumentDb
 
         [Fact]
         [Trait(Category.AcceptanceType, Category.CheckIn)]
-        public void GetDocumentDbDatabaseByIdFails()
+        public void GetDocumentDbDatabases_ReturnsEmptyListOfDatabases()
         {
             // Setup
-            cmdlet.Id = "test-db";
-            documentDbClientExtensionsMock.Setup(m => m.GetDatabase(It.IsAny<string>())).Returns<Database>(null); // The specified DocumentDb Database cannot be found
+            documentDbClientExtensionsMock.Setup(m => m.GetDatabases()).Returns(new List<ExtendedDocumentDbDatabase>()); // The specified DocumentDb Database cannot be found
 
             // Test
-            Testing.AssertThrows<Exception>(() => cmdlet.ExecuteCmdlet(), String.Format("Could not locate DocumentDb Database with the Id '{0}'", cmdlet.Id));
+            cmdlet.ExecuteCmdlet();
+
+            // Assert
+            IEnumerable<Database> actual = LanguagePrimitives.GetEnumerable(mockCommandRuntime.OutputPipeline).Cast<Database>();
+
+            Assert.NotNull(actual);
+            Assert.Equal<int>(0, actual.Count());
         }
     }
 }
